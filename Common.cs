5611ff73f85c2688363ee257537fb445b84bd25f
Kallikrates Koios <kallikrates.koios@gmail.com>
2013-09-17 17:19:46 -0400

    0522-17092013

--- Common.cs
+++ Common.cs
@@ -1,8 +1,44 @@
 using System;
+using System.Text;
 
 namespace Sabertooth.Lexicon {
-	public class Common {
-		public Common () {
+	public interface ITextable {string GetText();}
+	public interface IStreamable {byte[] GetBytes();}
+	public struct GenericTextable : ITextable {object text; public GenericTextable(object o){text = o;} public string GetText(){return text.ToString ();}}
+	public struct TextStreamable : IStreamable {
+		string text;
+		public TextStreamable(string t){
+			text = t;
+		}
+		public byte[] GetBytes(){
+			byte[] bytes = Encoding.UTF8.GetBytes(text);
+			return bytes;
+		}
+	}
+	public struct Statement : ITextable {
+		string Key;
+		string Value;
+		public Statement(string Key, string Value) {
+			this.Key = Key;
+			this.Value = Value;
+		}
+		public string GetText() {
+			return String.Format ("{0}={1}", Key, Value);
+		}
+		public override string ToString () {
+			return GetText ();
+		}
+	}
+	public class TimeTracker {
+		DateTime timeStart;
+		public TimeSpan Mark {
+			get{return DateTime.Now - timeStart;}
+		}
+		public TimeTracker() {
+			timeStart = DateTime.Now;
+		}
+		public void Restart() {
+			timeStart = DateTime.Now;
 		}
 	}
 }
--- MIME.cs
+++ MIME.cs
@@ -1,14 +1,14 @@
 using System;
 
 namespace Sabertooth.Lexicon {
-	public struct MIME {
+	public struct MIME : ITextable{
 		public readonly string Type;
 		public readonly string Format;
 		public MIME(string type, string format) {
 			Type = type;
 			Format = format;
 		}
-		public override string ToString () {
+		public string GetText () {
 			return String.Format ("{0}/{1}", Type, Format);
 		}
 		public static MIME Plaintext = new MIME("text", "plain");
